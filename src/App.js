39a5ec30d9b6931913321ee2ec941d3e670b619c
Chad Pjontek <chadpjontek@gmail.com>
2018-04-16 14:43:58 -0400

    init commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import * as d3 from "d3"
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Meteorite Landings Across the Globe</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
